683158e092f847cd398fe3a91cc0c4ac8a7acb69
krris <krrisgit@gmail.com>
2014-12-20 19:23:21 +0100

    Add aFileChooser library.

---
+++ app/src/main/java/io/github/krris/dicom/viewer/app/FileChooserExampleActivity.java
@@ -0,0 +1,8 @@
+package io.github.krris.dicom.viewer.app;
+
+/**
+ * Created by krris on 20.12.14.
+ * Copyright (c) 2014 krris. All rights reserved.
+ */
+public class FileChooserExampleActivity {
+}
