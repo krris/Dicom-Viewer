c7cd0c9780375d52a8633fa0fc7a431ae05bcd9e
krris <krrisgit@gmail.com>
2014-12-23 16:06:06 +0100

    Add file chooser dialog.

--- app/src/main/java/io/github/krris/dicom/viewer/app/MainActivity.java
+++ app/src/main/java/io/github/krris/dicom/viewer/app/MainActivity.java
@@ -3,17 +3,26 @@ package io.github.krris.dicom.viewer.app;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
+import com.ipaulpro.afilechooser.utils.FileUtils;
 
 /**
  * The launchpad activity for this sample project. This activity launches other activities that
  * demonstrate implementations of common animations.
  */
 public class MainActivity extends ListActivity {
+    private static final int REQUEST_CODE = 6384; // onActivityResult request
+    private static final String TAG = "FileChooserExampleActivity";
+
     /**
      * This class describes an individual sample (the sample title, and the activity class that
      * demonstrates this sample).
@@ -59,11 +68,58 @@ public class MainActivity extends ListActivity {
                 android.R.layout.simple_list_item_1,
                 android.R.id.text1,
                 mSamples));
+
+        final Button button = (Button) findViewById(R.id.choose_file_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Toast.makeText(MainActivity.this, "choose file", Toast.LENGTH_SHORT).show();
+                showChooser();
+            }
+        });
     }
 
     @Override
     protected void onListItemClick(ListView listView, View view, int position, long id) {
         // Launch the sample associated with this list position.
         startActivity(new Intent(MainActivity.this, mSamples[position].activityClass));
+    }
+
+    private void showChooser() {
+        // Use the GET_CONTENT intent from the utility class
+        Intent target = FileUtils.createGetContentIntent();
+        // Create the chooser Intent
+        Intent intent = Intent.createChooser(
+                target, getString(R.string.chooser_title));
+        try {
+            startActivityForResult(intent, REQUEST_CODE);
+        } catch (ActivityNotFoundException e) {
+            // The reason for the existence of aFileChooser
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CODE:
+                // If the file selection was successful
+                if (resultCode == RESULT_OK) {
+                    if (data != null) {
+                        // Get the URI of the selected file
+                        final Uri uri = data.getData();
+                        Log.i(TAG, "Uri = " + uri.toString());
+                        try {
+                            // Get the file path from the URI
+                            final String path = FileUtils.getPath(this, uri);
+                            Toast.makeText(MainActivity.this,
+                                    "File Selected: " + path, Toast.LENGTH_LONG).show();
+                        } catch (Exception e) {
+                            Log.e("FileSelectorTestActivity", "File select error", e);
+                        }
+                    }
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
