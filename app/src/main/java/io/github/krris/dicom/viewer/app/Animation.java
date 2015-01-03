3999f6c2f8a8c5c9b6e620eb63a91dd084a5794c
krris <krrisgit@gmail.com>
2015-01-03 10:33:31 +0100

    simple one dicom image.

--- app/src/main/java/io/github/krris/dicom/viewer/app/Animation.java
+++ app/src/main/java/io/github/krris/dicom/viewer/app/Animation.java
@@ -21,77 +21,65 @@ import java.util.TimerTask;
  * Copyright (c) 2015 krris. All rights reserved.
  */
 public class Animation extends Activity {
-    private DicomView dicomView;
-    private Timer timer;
-    private int index;
-    private MyHandler handler;
-
-    public void onCreate(Bundle savedInstanceState) {
+    // Called when the activity is first created.
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.animation_activity);
-
-        handler = new MyHandler();
-        dicomView = (DicomView) findViewById(R.id.dicomView);
-
-        index=0;
-        timer= new Timer();
-        timer.schedule(new TickClass(), 500, 200);
+        // LOAD THE LIBRARY
+        System.loadLibrary("imebra_lib");
     }
-
-    private class TickClass extends TimerTask {
-        @Override
-        public void run() {
-            handler.sendEmptyMessage(index);
-            index++;
-        }
-    }
-
-    private class MyHandler extends Handler {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-
-//                _imagView.setImageBitmap(bmp);
-            Log.i("Loaing Image: ", index + "");
-            addDicomImage();
-        }
-    }
-
-    private void addDicomImage() {
+    // Called when the activity starts
+    public void onStart()
+    {
+        super.onStart();
+        // Open the dicom file from sdcard
         Stream stream = new Stream();
-        stream.openFileRead("/storage/emulated/0/Download/dicom1.dcm");
-//        stream.openFileRead(medicalTest.getImages().getImage(mPageNumber));
+//        stream.openFileRead("/sdcard/Download/87FDH4G2.dcm");
+        stream.openFileRead("/storage/emulated/0/Download/pacjent1/tf2d15_retro_2ch_cine - 6/IM-0006-0001-0001.dcm");
         // Build an internal representation of the Dicom file. Tags larger than 256 bytes
         //  will be loaded on demand from the file
         DataSet dataSet = CodecFactory.load(new StreamReader(stream), 256);
         // Get the first image
         Image image = dataSet.getImage(0);
         // Monochrome images may have a modality transform
-        if(ColorTransformsFactory.isMonochrome(image.getColorSpace())) {
+        if(ColorTransformsFactory.isMonochrome(image.getColorSpace()))
+        {
             ModalityVOILUT modalityVOILUT = new ModalityVOILUT(dataSet);
-            if(!modalityVOILUT.isEmpty()) {
+            if(!modalityVOILUT.isEmpty())
+            {
                 Image modalityImage = modalityVOILUT.allocateOutputImage(image, image.getSizeX(), image.getSizeY());
                 modalityVOILUT.runTransform(image, 0, 0, image.getSizeX(), image.getSizeY(), modalityImage, 0, 0);
                 image = modalityImage;
             }
         }
-
+        // Just for fun: get the color space and the patient name
+        String colorSpace = image.getColorSpace();
+        String patientName = dataSet.getString(0x0010, 0, 0x0010, 0);
+        String dataType = dataSet.getDataType(0x0010, 0, 0x0010);
+        // Allocate a transforms chain: contains all the transforms to execute before displaying
+        //  an image
         TransformsChain transformsChain = new TransformsChain();
         // Monochromatic image may require a presentation transform to display interesting data
-        if(ColorTransformsFactory.isMonochrome(image.getColorSpace())) {
+        if(ColorTransformsFactory.isMonochrome(image.getColorSpace()))
+        {
             VOILUT voilut = new VOILUT(dataSet);
             int voilutId = voilut.getVOILUTId(0);
-            if(voilutId != 0) {
+            if(voilutId != 0)
+            {
                 voilut.setVOILUT(voilutId);
-            } else {
+            }
+            else
+            {
                 // No presentation transform is present: here we calculate the optimal window/width (brightness,
                 //  contrast) and we will use that
                 voilut.applyOptimalVOI(image, 0, 0, image.getSizeX(), image.getSizeY());
             }
             transformsChain.addTransform(voilut);
         }
-
-//        DicomView imageView = (DicomView) viewGroup.findViewById(R.id.imageView);
-        dicomView.setImage(image, transformsChain);
+        // Let's find the DicomView and se the image
+        DicomView imageView = (DicomView)findViewById(R.id.dicomView);
+        imageView.setImage(image, transformsChain);
     }
 }
